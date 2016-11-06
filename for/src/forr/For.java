e72b70215106367961499409b63e2209dc1166db
USUARIO <USUARIO@USUARIO-PC>
2016-11-06 10:31:24 -0300

    primercommit

---
+++ for/src/forr/For.java
@@ -0,0 +1,17 @@
+package forr;
+
+public class For {
+
+	public static void main(String[] args) {
+		//int i = 0;
+		//while (i<3){
+			//System.out.println(i);
+			//i++;
+		//}
+		for (int i = 0; i < 3; i++) {
+			System.out.println(i);
+		
+
+	}
+
+	}}
